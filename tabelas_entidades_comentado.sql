171e306c28b0735ece8179358db8accca3caebed
Victor Luiz <86299194+victorluizz@users.noreply.github.com>
2022-08-30 21:46:18 -0300

    Update tabelas_entidades_comentado.sql
    Tirando a sequência  de reply.

--- tabelas_entidades_comentado.sql
+++ tabelas_entidades_comentado.sql
@@ -133,12 +133,6 @@ CREATE TABLE bane(
 
 -- Criando as sequencias
 
-CREATE SEQUENCE reply_numero_seq -- Criando uma sequência para a chave primária "numero" da tabela "reply"
-    MINVALUE 1
-    INCREMENT BY 1
-    START WITH 1
-    CACHE 50;
-
 CREATE SEQUENCE card_numero_seq -- Criando uma sequência para a chave primária "numero" da tabela "cards_do_usuario"
     MINVALUE 1
     INCREMENT BY 1

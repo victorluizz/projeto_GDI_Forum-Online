b21dcdd1548f2cd67aae4303a942fd375eb5c118
Carlos <crcp@cin.ufpe.br>
2022-10-04 12:21:29 -0300

    tabelas ternarias

--- SGBDOR/criacao_OR.sql
+++ SGBDOR/criacao_OR.sql
@@ -1,11 +1,23 @@
 drop table tb_usuarios;
 drop table tb_moderadores;
+drop table tb_replies;
+drop table tb_secoes;
+drop table tb_threads;
+drop table tb_cria_thread;
+drop table tb_cria_reply;
+drop table tb_bane;
+
+drop type tp_bane;
+drop type tp_cria_thread;
+drop type tp_cria_reply;
 drop type tp_moderador;
 drop type tp_usuario;
 drop type tp_endereco;
 drop type tp_emails;
 drop type tp_email;
 
+
+-- USUARIOS
 CREATE OR REPLACE TYPE tp_email AS OBJECT (
     endereco_email VARCHAR2(50)
 );
@@ -51,10 +63,75 @@ CREATE TABLE tb_moderadores OF tp_moderador (
 /
 
 
+-- THREADS
+CREATE OR REPLACE TYPE tp_thread AS OBJECT(
+    id_thread NUMBER
+    ,titulo VARCHAR2(50)
+    ,texto VARCHAR2(300)  
+);
+/
+
+CREATE TABLE tb_threads OF tp_thread(
+    id_thread primary key
+);
+/
+
+-- REPLIES
+CREATE OR REPLACE TYPE tp_reply AS OBJECT(
+    numero NUMBER
+    ,mensagem VARCHAR2(300)
+);
+/
+
+CREATE TABLE tb_replies OF tp_reply(
+    numero primary key
+);
+/
+
+-- SECOES
+CREATE OR REPLACE TYPE tp_secao AS OBJECT (
+    id_secao NUMBER
+    ,titulo VARCHAR2(40)
+);
+/
+
+CREATE TABLE tb_secoes OF tp_secao(
+    id_secao primary key
+);
+/
+
+-- CRIACAO DE POSTAGEM (TERNARIO)
+CREATE OR REPLACE TYPE tp_cria_thread AS OBJECT(
+    login REF tp_usuario,
+    id_thread REF tp_thread,
+    id_secao REF tp_secao,
+    data_hora TIMESTAMP
+);
+/
+
+CREATE TABLE tb_cria_thread OF tp_cria_thread;
 
 
+-- CRIACAO DE REPLY (TERNARIO)
+CREATE OR REPLACE TYPE tp_cria_reply AS OBJECT(
+    login REF tp_usuario,
+    id_thread REF tp_thread,
+    numero REF tp_reply,
+    data_hora TIMESTAMP
+);
+/
 
+CREATE TABLE tb_cria_reply OF tp_cria_reply;
 
+-- BANIR (TERNARIO)
+CREATE OR REPLACE TYPE tp_bane AS OBJECT(
+    login_usario REF tp_usuario,
+    login_moderador REF tp_moderador,
+    id_secao REF tp_secao
+);
+/
+
+CREATE TABLE tb_bane OF tp_bane;
 
 
 
@@ -79,6 +156,24 @@ insert into tb_usuarios values(
     tp_emails(tp_email('email1@gmail.com'),tp_email('email2@hotmail.com')))
 );
 
+insert into tb_usuarios values(
+    tp_usuario(
+    'victor',
+    null,
+    '555',
+    'Victor Luiz',
+    'aaaB',
+    tp_endereco(
+        15,
+        '456789123',
+        'Pernambuco',
+        'rua do reitor',
+        'Recife'
+        )
+    ,
+    tp_emails(tp_email('victor@gmail.com'),tp_email('victor@hotmail.com')))
+);
+
 insert into tb_moderadores values(
     tp_moderador(
     'Joao',
@@ -98,5 +193,50 @@ insert into tb_moderadores values(
     1)
 );
 
+insert into tb_secoes values(
+    tp_secao(
+    1,
+    'Ação'
+    )
+);
+insert into tb_secoes values(
+    tp_secao(
+    2,
+    'Comédia'
+    )
+);
+
+insert into tb_threads values(
+    tp_thread(
+    1,
+    'Velozes e furiosos 12',
+    'Pessoal, o que acharam do novo velozes e furiosos?'  
+    )
+);
+insert into tb_threads values(
+    tp_thread(
+    2,
+    'Comedias do netflix',
+    'Me rencomdem filmes de comédia no netflix por favor'  
+    )
+);
+
+insert into tb_cria_thread values(
+    (select ref (P) from tb_usuarios P where login ='carlos'),
+    (select ref (G) from tb_threads G where id_thread = 1),
+    (select ref (Z) from tb_secoes Z where id_secao = 1),
+    null
+);
+
+insert into tb_cria_thread values(
+    (select ref (P) from tb_usuarios P where login ='victor'),
+    (select ref (G) from tb_threads G where id_thread = 2),
+    (select ref (Z) from tb_secoes Z where id_secao = 2),
+    null
+);
+
 select * from tb_usuarios;
 select * from tb_moderadores;
+select * from tb_secoes;
+select * from tb_threads;
+select * from tb_cria_thread;

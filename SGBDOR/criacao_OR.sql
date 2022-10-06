9c3c28f12d4ccb80b541f8ffd203881c9d6c29b7
Victor Luiz <86299194+victorluizz@users.noreply.github.com>
2022-10-05 22:46:17 -0300

    Mudança final

--- SGBDOR/criacao_OR.sql
+++ SGBDOR/criacao_OR.sql
@@ -172,7 +172,7 @@ CREATE TABLE tb_secoes OF tp_secao(
 -- CRIACAO DE POSTAGEM (TERNARIO)
 CREATE OR REPLACE TYPE tp_cria_thread AS OBJECT(
     id_cria_thread number,
-    login REF tp_usuario,
+    login REF tp_pessoa,
     id_thread REF tp_thread,
     id_secao REF tp_secao,
     data_hora TIMESTAMP,
@@ -197,17 +197,14 @@ END;
 /
 
 CREATE TABLE tb_cria_thread OF tp_cria_thread(
-    id_cria_thread primary key,
-    login with rowid references tb_usuarios,
-    id_thread with rowid references tb_threads,
-    id_secao scope is tb_secoes
+    id_cria_thread primary key
 );
 
 
 -- CRIACAO DE REPLY (TERNARIO)
 CREATE OR REPLACE TYPE tp_cria_reply AS OBJECT(
     id_cria_reply number,
-    login REF tp_usuario,
+    login REF tp_pessoa,
     id_thread REF tp_thread,
     numero REF tp_reply,
     data_hora TIMESTAMP
@@ -215,15 +212,12 @@ CREATE OR REPLACE TYPE tp_cria_reply AS OBJECT(
 /
 
 CREATE TABLE tb_cria_reply OF tp_cria_reply(
-    id_cria_reply primary key,
-    login with rowid references tb_usuarios,
-    id_thread with rowid references tb_threads,
-    numero with rowid references tb_replies
+    id_cria_reply primary key
 );
 
 -- BANIR (TERNARIO)
 CREATE OR REPLACE TYPE tp_bane AS OBJECT(
-    login_usuario REF tp_usuario,
+    login_usuario REF tp_pessoa,
     login_moderador REF tp_moderador,
     id_secao REF tp_secao
 );
@@ -289,160 +283,3 @@ CREATE TABLE tb_modera_thread OF tp_modera_thread(
     id_modera primary key
 );
 /
-
--- TESTE POVOAMENTO
-insert into tb_moderadores values(
-    tp_moderador(
-    'Joao',
-    null,
-    '321',
-    'Joao Pereira',
-    'bbbbb',
-    tp_endereco(
-        15,
-        '999999999',
-        'Pernambuco',
-        'Av boa viagem',
-        'Recife'
-        )
-    ,
-    tp_emails(tp_email('joaomod@gmail.com'),tp_email('joaomod2@hotmail.com')),
-    tp_lista_cards(),
-    1
-    )
-);
-
--- CARDS
-INSERT INTO TABLE (SELECT U.lista_cards FROM tb_usuarios U WHERE
-U.login = 'carlos') T VALUES (tp_card_usuario (1, 'Ola!', 'tenha um bom dia'));
-/
-
-INSERT INTO TABLE (SELECT U.lista_cards FROM tb_usuarios U WHERE
-U.login = 'carlos') T VALUES (tp_card_usuario (2, 'Xau!', 'boa noite!'));
-/
-
--- MENSAGENS
-    
-INSERT INTO tb_envia_mensagem
-VALUES (tp_envia_mensagem
-(   (select ref (E) from tb_usuarios E where login ='carlos'),
-    (select ref (R) from tb_usuarios R where login ='victor'),
-    null,
-    'me empresta tua conta do netflix',
-    1
-));
-
-INSERT INTO tb_envia_mensagem
-VALUES (tp_envia_mensagem
-(   (select ref (E) from tb_usuarios E where login ='victor'),
-    (select ref (R) from tb_usuarios R where login ='carlos'),
-    null,
-    'NAO',
-    2
-));
-
-INSERT INTO tb_envia_mensagem
-VALUES (tp_envia_mensagem
-(   (select ref (E) from tb_usuarios E where login ='victor'),
-    (select ref (R) from tb_usuarios R where login ='carlos'),
-    null,
-    'Cria uma conta pra tu',
-    3
-));
-
--- ANEXOS
-INSERT INTO tb_anexos VALUES (tp_anexo
-(
-    (select ref (M) from tb_envia_mensagem M where numero = 3),
-    'netflix.com',
-    1
-
-));
-
-
--- SECOES
-insert into tb_secoes values(
-    tp_secao(
-    1,
-    'Ação'
-    )
-);
-insert into tb_secoes values(
-    tp_secao(
-    2,
-    'Comédia'
-    )
-);
-
--- THREADS
-insert into tb_threads values(
-    tp_thread(
-    1,
-    'Velozes e furiosos 12',
-    'Pessoal, o que acharam do novo velozes e furiosos?'  
-    )
-);
-insert into tb_threads values(
-    tp_thread(
-    2,
-    'Comedias do netflix',
-    'Me rencomdem filmes de comédia no netflix por favor'  
-    )
-);
-
-insert into tb_cria_thread values(
-    (select ref (P) from tb_usuarios P where login ='carlos'),
-    (select ref (G) from tb_threads G where id_thread = 1),
-    (select ref (Z) from tb_secoes Z where id_secao = 1),
-    null,
-    1
-);
-
-insert into tb_cria_thread values(
-    (select ref (P) from tb_usuarios P where login ='victor'),
-    (select ref (G) from tb_threads G where id_thread = 2),
-    (select ref (Z) from tb_secoes Z where id_secao = 2),
-    null,
-    2
-);
-
--- MODERACAO
-
-INSERT INTO tb_modera_secao VALUES(
-    (SELECT REF (M) FROM tb_moderadores M WHERE login = 'Joao'),
-    (SELECT REF (S) FROM tb_secoes S WHERE id_secao = 1),
-    1
-);
-INSERT INTO tb_modera_secao VALUES(
-    (SELECT REF (M) FROM tb_moderadores M WHERE login = 'Joao'),
-    (SELECT REF (S) FROM tb_secoes S WHERE id_secao = 2),
-    2
-);
-
-INSERT INTO tb_modera_thread VALUES(
-    (SELECT REF (M) FROM tb_moderadores M WHERE login = 'Joao'),
-    (SELECT REF (T) FROM tb_threads T WHERE id_thread = 2),
-    'editar',
-    1
-);
-
-
-select * from tb_usuarios;
-select U.login, U.data_hora, U.senha, U.nome, u.foto_de_perfil, (U.endereco).cep, (U.endereco).estado,
-(U.endereco).cidade, (U.endereco).rua, (U.endereco).numero from tb_usuarios U;
-
-SELECT E.endereco_email carlos_emails FROM tb_usuarios U, TABLE (U.emails) E WHERE U.login = 'carlos';
-
-SELECT 'carlos' as Usuario_Cards, T.numero, T.titulo, T.descricao FROM TABLE (SELECT U.lista_cards FROM tb_usuarios U WHERE U.login = 'carlos') T;
-/
-
-SELECT DEREF(M.login_envia).login as login_envia, DEREF(M.login_recebe).login as login_recebe, M.data_hora, M.texto, M.numero FROM tb_envia_mensagem M;
-SELECT DEREF(DEREF(A.numero_msg).login_envia).login as login_envia, DEREF(DEREF(A.numero_msg).login_recebe).login as login_recebe, DEREF(A.numero_msg).texto as mensagem, A.link_anexo, A.id_anexo FROM tb_anexos A;
-SELECT DEREF(M.login_moderador).login as MODERADOR_LOGIN, DEREF(M.id_secao).titulo as SECAO_TITULO, M.id_modera_secao FROM tb_modera_secao M; 
-SELECT DEREF(M.login_moderador).login as MODERADOR_LOGIN, DEREF(M.id_thread).titulo as THREAD_TITULO, M.acao, M.id_modera FROM tb_modera_thread M; 
-
-select * from tb_moderadores;
-select * from tb_secoes;
-select * from tb_threads;
-SELECT DEREF(C.login).login as Criador, DEREF(C.id_thread).titulo as Titulo, DEREF(C.id_secao).titulo as Secao, 
-C.data_hora as Data_Criacao FROM tb_cria_thread C;
